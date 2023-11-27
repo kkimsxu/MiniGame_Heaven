603d43f9483fd06cf7967e5380f0f4d0ab3ff5d0
kkimsxu <i01023730567@gmail.com>
2023-11-28 01:26:14 +0900

    add src 'BeadsGame'

---
+++ MarbleGame/BeadsGame/MarbleGame.java
@@ -0,0 +1,10 @@
+package GlassBeadGame;
+
+public class MarbleGame {
+    public static void main(String[] args) {
+        Player p = new Player();
+        Computer c = new Computer();
+        Registrar r = new Registrar();
+        new GameController(p, c, r).play();
+    }
+}
