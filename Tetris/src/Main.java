549a397f90853cdd78e71e44c140e847284ea071
gimyeonjik <102420297+gimyeonjik@users.noreply.github.com>
2023-11-28 13:54:23 +0900

    Add Tetris File[Tetris]

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -1,16 +1,16 @@
-// Shift을(를) 두 번 눌러 전체 검색 대화상자를 열고 'show whitespaces'를 입력한 다음,
-// Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // 캐럿을 강조 표시된 텍스트에 놓고 Alt+Enter을(를) 누르면
-        // IntelliJ IDEA의 수정 제안을 볼 수 있습니다.
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        // Shift+F10을(를) 누르거나 여백의 녹색 화살표 버튼을 클릭하여 코드를 실행합니다.
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
-            // Shift+F9을(를) 눌러 코드 디버그를 시작합니다. 중단점을 하나 설정해 드렸습니다.
-            // 중단점을 더 추가하려면 언제든지 Ctrl+F8을(를) 누르세요.
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
         }
     }
