08ca19446f844e75d243064d7a398694f3af9afc
newblackrabbit <cy0410@hanyang.ac.kr>
2023-12-05 00:54:47 +0900

    week2 code

---
+++ _02_SlidePuzzleBoard/src/_11_SlidePuzzleBoard_copy_plz/PuzzleButton.java
@@ -0,0 +1,30 @@
+package _11_SlidePuzzleBoard_copy_plz;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+class PuzzleButton extends JButton implements ActionListener {
+    private SlidePuzzleBoard board;
+    private PuzzleFrame frame;
+
+    public PuzzleButton(SlidePuzzleBoard b, PuzzleFrame f) {
+        board = b;
+        frame = f;
+        addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (board.on()) {
+            String s = getText();
+            if (!s.equals("") && board.move(Integer.parseInt(s))) {
+                frame.update();
+                if (board.gameOver()) {
+                    frame.finish();
+                    frame.stopTimer();
+                }
+            }
+        }
+    }
+}
+
+
---
+++ _02_SlidePuzzleBoard/src/_11_SlidePuzzleBoard_copy_plz/PuzzlePiece.java
@@ -0,0 +1,13 @@
+package _11_SlidePuzzleBoard_copy_plz;
+
+class PuzzlePiece {
+    private int face;
+
+    public PuzzlePiece(int value) {
+        face = value;
+    }
+
+    public int face() {
+        return face;
+    }
+}
